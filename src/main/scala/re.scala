09e0f2170050cbfec867ff5dd83f52394aa58878
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-08-29 18:29:09 -0600

    add: 'all' RegexParser expressions, helper methods

--- src/main/scala/re.scala
+++ src/main/scala/re.scala
@@ -1,3 +1,6 @@
+import scala.annotation.tailrec
+import scala.util.parsing.combinator._
+
 abstract class RegexExpr
 
 case class Literal(c: Char)                       extends RegexExpr // a, b
@@ -5,4 +8,38 @@ case class Or(exprA: RegexExpr, exprB: RegexExpr) extends RegexExpr // a | b
 case class Concat(a: RegexExpr, b: RegexExpr)     extends RegexExpr // abc -> Concat(a, Concat(b, c))
 case class Repeat(expr: RegexExpr)                extends RegexExpr // a*
 case class Plus(expr: RegexExpr)                  extends RegexExpr // a+
+
+/* operator binding order (strongest -> weakest):
+ (1. char literals, parentheses)
+  2. +, *
+  3. concatenation
+  4. |
+*/
+
+object RegexParser extends RegexParsers {
+
+    def exprCharLiteral:   Parser[RegexExpr] = ("""\w""".r | ".") ^^ {c => Literal(c.head)}
+    def exprParenthetical: Parser[RegexExpr] = "(" ~> prioLow <~ ")"
+    def exprRepeat:        Parser[RegexExpr] = prioLow <~ "*" ^^ (l => Repeat(l))
+    def exprPlus:          Parser[RegexExpr] = prioLow <~ "+" ^^ (l => Plus(l))
+    def exprConcat:        Parser[RegexExpr] = rep(prioMid) ^^ {list => listToConcat(list)}
+    def exprOr:            Parser[RegexExpr] = prioHigh ~ "|" ~ prioHigh ^^ {case l ~ "|" ~ r => Or(l, r)}
+
+    def prioLow:  Parser[RegexExpr] = exprCharLiteral | exprParenthetical
+    def prioMid:  Parser[RegexExpr] = exprRepeat | exprPlus | prioLow
+    def prioHigh: Parser[RegexExpr] = exprConcat | prioMid
+    def prioMax:  Parser[RegexExpr] = exprOr | prioHigh
+
+    def listToConcat(list: List[RegexExpr]): RegexExpr = list match {
+        case head :: Nil       => head
+        case head :: remainder => Concat(head, listToConcat(remainder))
+    }
+
+    def apply(in: String): Option[RegexExpr] = {
+        parseAll(prioMax, in) match {
+            case Success(out, _)    => Some(out)
+            case failure: NoSuccess => None
+        }
+    }
+}
 
