56762af4e5357fc2176ee5b4eba6a487a7b5548c
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-09-09 14:44:59 -0600

    rev [re]: comment format oopsie

--- src/main/scala/re.scala
+++ src/main/scala/re.scala
@@ -4,7 +4,7 @@ import scala.util.parsing.combinator._
 
 abstract class RegexExpr
 
-case class Literal(c: Char)                       extends RegexExpr // e.g. a, b, .
+case class Literal(c: Char)                       extends RegexExpr // a, b, .
 case class Or(exprA: RegexExpr, exprB: RegexExpr) extends RegexExpr // a | b
 case class Concat(a: RegexExpr, b: RegexExpr)     extends RegexExpr // abc -> Concat(a, Concat(b, c))
 case class Repeat(expr: RegexExpr)                extends RegexExpr // a*
